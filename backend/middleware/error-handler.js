849d014f468236c8469283649b1b99a05c71b886
Lợi <loivantran10012002@gmail.com>
2023-07-20 13:30:12 +0700

    update 1 - api auth: reg/login

--- backend/middleware/error-handler.js
+++ backend/middleware/error-handler.js
@@ -6,6 +6,18 @@ const errorHandlerMiddleWare = (err, req, res, next) => {
         statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
         msg: err.message || 'Something was wrong, please try later'
     }
+    console.log(err.name)
+    console.log(err.code)
+    if (err.name === 'ValidationError') {
+        customError.msg = Object.values(err.errors)
+            .map((item) => item.message)
+            .join(', ')
+        customError.statusCode = 400
+    }
+    if (err.code && err.code === 11000) {
+        customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please choose another value`
+        customError.statusCode = 400
+    }
     return res.status(customError.statusCode).json({ msg: customError.msg })
 }
 
--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const { connectDB } = require('./configs/db')
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleWare = require('./middleware/error-handler')
 
+const authRouter = require('./routes/auth')
 const productsRouter = require('./routes/products')
 
 app.use(express.json())
@@ -16,6 +17,8 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.send('Chào !!!')
 })
+
+app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/products', productsRouter)
 
 app.use(notFoundMiddleware)
