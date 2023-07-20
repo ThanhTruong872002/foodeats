eff94c8af623ad05efaecd94d1ab24360b6eafcd
School <118030762+nguyenduyquang0@users.noreply.github.com>
2023-07-20 20:25:24 +0700

    add cors BE

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,36 @@
+require("dotenv").config();
+require("express-async-errors");
+var cors = require("cors");
+const express = require("express");
+const app = express();
 
-require('dotenv').config()
-require('express-async-errors')
+const { connectDB } = require("./configs/db");
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleWare = require("./middleware/error-handler");
 
-const express = require('express')
-const app = express()
+const authRouter = require("./routes/auth");
+const productsRouter = require("./routes/products");
 
-const { connectDB } = require('./configs/db')
-const notFoundMiddleware = require('./middleware/not-found')
-const errorHandlerMiddleWare = require('./middleware/error-handler')
+app.use(express.json());
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("Chào !!!");
+});
 
-const authRouter = require('./routes/auth')
-const productsRouter = require('./routes/products')
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/products", productsRouter);
 
-app.use(express.json())
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleWare);
 
-app.get('/', (req, res) => {
-    res.send('Chào !!!')
-})
-
-app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/products', productsRouter)
-
-app.use(notFoundMiddleware)
-app.use(errorHandlerMiddleWare)
-
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URI)
-        app.listen(PORT, console.log(`Server listenning on port ${PORT}...`))
-    }
-    catch (err) {
-        console.log('Error while start server: ' + err)
-    }
-}
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, console.log(`Server listenning on port ${PORT}...`));
+  } catch (err) {
+    console.log("Error while start server: " + err);
+  }
+};
 
-start()
+start();
